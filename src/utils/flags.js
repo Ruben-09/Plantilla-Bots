df399b7c17b2b886d9b5a6d1b3daff61976624c8
Ruben <50890330+Ruben-09@users.noreply.github.com>
2020-10-07 10:34:32 -0400

    Create flags.js

---
+++ src/utils/flags.js
@@ -0,0 +1,59 @@
+module.exports = {
+    Badges: {
+        'DISCORD_EMPLOYEE':'<:DiscordStaff:730664032419315773>',
+        'DISCORD_PARTNER':'<:DiscordPartner:730664032675299358>',
+        'HYPESQUAD_EVENTS':'<:HypeSquad:730664032096354406>',
+        'HOUSE_BRAVERY':'<:HypeSquadBravery:730664032792608849>',
+        'HOUSE_BRILLIANCE':'<:HypeSquadBrilliance:730664032423379004>',
+        'HOUSE_BALANCE':'<:HypeSquadBalance:730664031937101921>',
+        'EARLY_SUPPORTER':'<:DiscordNitroEarlySupporter:730664032662585414>',
+        'TEAM_USER':'<:Team_User:740667003479785534>',
+        'SYSTEM':'',
+        'VERIFIED_BOT':'<:Bot_Verificado:754770315258757181>',
+        'VERIFIED_DEVELOPER':'<:VerifiedBotDeveloper:730664032591413299>',
+        'BUGHUNTER_LEVEL_1':'<:DiscordBugHunter:730664032305938444>',
+        'BUGHUNTER_LEVEL_2':'<:9552_BugHunterLvl2:735355374999306311>',
+    },
+    Flags: {
+        'us-west' : ':flag_us: Costa Oeste de los Estados Unidos',
+        'us-east' : ':flag_us: Costa Este de los Estados Unidos',
+        'us-central' : ':flag_us: Centro de Estados Unidos ',
+        'us-south' : 'flag_us: Sur de Estados Unidos',
+        'singapore' : ':flag_si: Singapur', 
+        'southafrica' : ':flag_za: Sudáfrica',
+        'sydney' : ':flag_hm: Sidney',
+        'europe' : ':flag_eu: Europa',
+        'brazil' : ':flag_br: Brazil',
+        'hongkong' : ':flag_hk: Hong Kong',
+        'russia' : ':flag_ru: Rusia',
+        'japan' : ':flag_jp: Japan',
+        'india' : ':flag_in: India'
+    },
+    Features: {
+        'ANIMATED_ICON' : 'Icono Animado',
+        'BANNER' : 'Banner',
+        'COMMERCE' : 'Comercio',
+        'COMMUNITY' : 'Comunidad',
+        'DISCOVERABLE' : 'Descubrirle',
+        'FEATURABLE' : '',
+        'INVITE_SPLASH' : 'Splash de Invitación',
+        'NEWS' : 'Noticias',
+        'PARTNERED' : 'Partner',
+        'VANITY_URL' : 'URL Personalizable',
+        'VERIFIED' : 'Verificado',
+        'VIP_REGIONS' : 'Región VIP',
+        'WELCOME_SCREE_ENABLED' : 'Pantalla de Bievenida'
+    },
+    Notificaciones: {
+        'ALL':'Todos los mensajes',
+        'MENTIONS':'Solo @mentions'
+    },
+    Verificacion: {
+        'NONE':'Ninguno',
+        'LOW':'Bajo',
+        'MEDIUM':'Medio',
+        'HIGH':'Alto',
+        'VERY_HIGH':'Muy Alto'
+    }
+
+}
